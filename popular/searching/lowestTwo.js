eafce766b15624c871b074a81ca8586c92c6b1b6
SaiKishoret99 <saikishoret99@gmail.com>
2018-07-10 11:14:41 +0530

    solved lowest two

--- popular/searching/lowestTwo.js
+++ popular/searching/lowestTwo.js
@@ -1,11 +1,23 @@
 function findIndex(array){
 	var lowest = 0;
 	var secondLowest  = 0;
-	array.reduce((a,c,i) =>{
+	array.map((c,i) =>{
 		if(c < lowest){
 			secondLowest = lowest
 			lowest = c;
-
 		}	
 	})
+
+	return {
+		lowest: lowest,
+		secondLowest: secondLowest
+	}
 }
+
+var a = [2, 5, -1, 5, 6,-2]
+var response = findIndex(a);
+console.log(response)
+
+
+
+
